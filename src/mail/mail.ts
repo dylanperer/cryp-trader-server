53896511b43cf8c59a19b1535fe3de52261a7dc2
dylanperer <dylan.perer29@protonmail.com>
2023-04-23 17:09:55 +1200

    ++

--- src/mail/mail.ts
+++ src/mail/mail.ts
@@ -1,62 +1,63 @@
-// import {
-//   MailListener,
-//   IMailAttachment,
-//   IMailObject,
-// } from "mail-listener-typescript";
+import { OAuth2Client } from "google-auth-library";
+const nodemailer = require("nodemailer");
+const { google } = require("googleapis");
 
-// const options = {
-//   username: "xxx@outlook.com", // mail
-//   password: "password", // pass
-//   host: "outlook.office365.com", // host
-//   port: 993, // imap port
-//   tls: true, // tls
-//   connTimeout: 10000, // Default by node-imap
-//   authTimeout: 5000, // Default by node-imap,
-//   debug: console.log(), // Or your custom function with only one incoming argument. Default: null
-//   tlsOptions: { rejectUnauthorized: false },
-//   mailbox: "INBOX", // mailbox to monitor
-//   searchFilter: ["NEW"], // the search filter being used after an IDLE notification has been retrieved
-//   markSeen: false, // all fetched email will be marked as seen and not fetched next time
-//   fetchUnreadOnStart: true, // use it only if you want to get all unread email on lib start. Default is `false`,
-//   mailParserOptions: { streamAttachments: true }, // options to be passed to mailParser lib.
-//   attachments: true, // get mail attachments as they are encountered
-//   attachmentOptions: {
-//     saveAttachments: false, // save attachments to the project directory
-//     directory: "attachments/", // folder on project directory to save attachements, will be created if not exists
-//     stream: true, // if it's enabled, will stream the attachments
-//   },
-// };
+const CLIENT_ID =
+  "362883414554-7hunsis61ulc4lt26l026sjetgttmc48.apps.googleusercontent.com";
+const CLIENT_SECRET = "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";
+const REDIRECT_URI = "https://developers.google.com/oauthplayground";
+const REFRESH_TOKEN =
+  "1//04u_NX2VCagghCgYIARAAGAQSNwF-L9IrdJUtsT2NCPcUNyLiUlid2FjKBsMRI3gS8_kx2lCkyPARDFzLTQEtustwOD6pQlYuqv0";
 
-// export const mailListener = new MailListener(options);
+const oAuth2Client = new google.auth.OAuth2(
+  CLIENT_ID,
+  CLIENT_SECRET,
+  REDIRECT_URI
+);
 
+oAuth2Client.setCredentials({ refresh_token: REFRESH_TOKEN });
 
+export const sendMail = async () => {
+  try {
+    const accessToken = await oAuth2Client.getAccessToken();
 
-// // Start
-// mailListener.start();
+    const transport = nodemailer.createTransport({
+      service: "gmail",
+      auth: {
+        type: "OAuth2",
+        user: "cryp.trader01@gmail.com",
+        clientId: CLIENT_ID,
+        clientSecret: CLIENT_SECRET,
+        refreshToken: REFRESH_TOKEN,
+        accessToken: accessToken,
+      },
+    });
 
-// // Simple example of how to get all attachments from an email
-// mailListener.on(
-//   "mail",
-//   async function (mail: IMailObject, seqno: any, attributes: any) {
-//     if (mail.attachments.length > 0) {
-//       var mailAttachments: IMailAttachment[] = [];
+    const mailOptions = {
+      from: "cryp-trader <cryp.trader01@gmail.com>",
+      to: "cryp.trader01@gmail.com",
+      subject: "api test",
+      text: "hello",
+    };
 
-//       for (let attachment of mail.attachments) {
-//         mailAttachments.push(attachment);
-//       }
-//       let mailData = {
-//         name: mail.from.value[0].name,
-//         sender: mail.from.value[0].address,
-//         date: new Date(mail.date),
-//         files: mailAttachments,
-//       };
+    const result = await transport.sendMail(mailOptions);
+    return result;
+  } catch (err) {
+    return err;
+  }
+};
 
-//       console.log(mailData);
-//     }
-//   }
-// );
+// Listen for new email messages
+async function watchForNewEmails() {
+  const gmail = google.gmail({ version: "v1", auth: oAuth2Client });
 
-// // Get erros
-// mailListener.on("error", async function (error: any) {
-//   console.log("Mail listener Error", error);
-// });
+  const res = await gmail.users.watch({
+    userId: "me",
+    resource: {
+      topicName: "YOUR_PUBSUB_TOPIC_NAME_HERE", // Replace this with the name of your Pub/Sub topic
+      labelIds: ["INBOX"],
+    },
+  });
+
+  console.log(res.data);
+}
--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,8 @@
+import { sendMail } from "./mail/mail";
+
 console.log('HELLO FROM SERVER');
+
+// Call the authorize() and watchForNewEmails() functions
+sendMail()
+  .then((result) => console.log("Email sent...", result))
+  .catch((error) => console.log(error.message));
