b3f5d7166266c510291d9407532f35271af8c3e5
dylanperer <dylan.perer29@protonmail.com>
2023-04-24 20:49:11 +1200

    update 7 files

--- dist/server.js
+++ dist/server.js
@@ -1,7 +1,5 @@
 import dotenv from "dotenv";
 import { addMailListener } from "./mail/mail.js";
 dotenv.config();
-
-console.log(process.env.EMAIL_PASSWORD);
 addMailListener(process.env.EMAIL_ADDRESS || "", process.env.EMAIL_PASSWORD || "");
 //# sourceMappingURL=server.js.map
