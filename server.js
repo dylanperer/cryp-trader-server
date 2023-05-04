3f2bf4f1fcb80b002f21b8a23b2608d73fd6f6ef
dylanperer <dylan.perer29@protonmail.com>
2023-05-04 22:40:31 +1200

    update 11 files

--- server.js
+++ server.js
@@ -13,17 +13,18 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const dotenv_1 = __importDefault(require("dotenv"));
+const mail_1 = require("./src/mail");
 const logger_1 = require("./src/logger");
 const prisma_1 = require("./prisma/prisma");
-const binance_1 = require("./src/binance/binance");
+const api_1 = require("./src/api");
 dotenv_1.default.config();
 const configureServer = () => __awaiter(void 0, void 0, void 0, function* () {
     try {
         yield (0, prisma_1.endPreviousSession)();
         yield (0, prisma_1.createSession)();
-        // startExpress();
-        // await addMailListener();
-        yield (0, binance_1.connectToBinance)();
+        (0, api_1.startExpress)();
+        yield (0, mail_1.addMailListener)();
+        // await connectToBinance();
     }
     catch (error) {
         (0, logger_1.serverError)(logger_1.ModuleType.Server, logger_1.ActionType.serverStart, `${error.message}`);
